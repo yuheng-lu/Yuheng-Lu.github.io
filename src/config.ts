707c361cebc0c43796720222ab90ac2314f1bb70
Yuheng Lu <lyh2002329@gmail.com>
2026-06-19 19:05:37 -0400

    Improve site SEO

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "about me";
+export const SITE_TITLE = "Yuheng Lu";
 export const SITE_DESCRIPTION =
-  "personal website of yuheng lu, software engineer and nyu courant alum";
+  "Yuheng Lu is a software engineer at Duolingo and an NYU Courant alumnus.";
+export const SITE_OWNER = "Yuheng Lu";
+export const SITE_URL = "https://yuheng-lu.github.io";
+export const SOCIAL_LINKS = [
+  "https://github.com/Yuheng-Lu",
+  "https://www.linkedin.com/in/yuheng-lu",
+];
 export const TRANSITION_API = true;
